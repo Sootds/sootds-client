831b0af620d58f2597534513bcbab7159a02bc96
AJ Clemente <aj.clemente@sootds.com>
2021-06-15 14:50:14 -0400

    Add `Roboto` font from Google Fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,22 @@
-import React, { FunctionComponent, memo } from 'react';
+import React, { FunctionComponent, Fragment, memo } from 'react';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 // Component
 const App: FunctionComponent<AppProps> = (props: AppProps) => (
-  <>
+  <Fragment>
+    <Head>
+      <link rel='preconnect' href='https://fonts.gstatic.com' />
+      <link
+        href='https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700;1,900&display=swap'
+        rel='stylesheet'
+      />
+    </Head>
     <GlobalStyle />
     <props.Component {...props.pageProps} />
-  </>
+  </Fragment>
 );
 
 // Styles
@@ -24,6 +32,7 @@ const GlobalStyle = createGlobalStyle`
     height: 100vh;
     margin: 0;
     padding: 0;
+    font-family: 'Roboto', sans-serif;
   }
 
   #__next {
