87332c193da14a64ca3c58bb963cecf7ce3b84bc
AJ Clemente <aj.clemente@sootds.com>
2021-08-19 21:01:03 -0400

    Update personal info form functionality

--- src/containers/CreateVendorStore/components/PersonalInfoForm/PersonalInfoForm.tsx
+++ src/containers/CreateVendorStore/components/PersonalInfoForm/PersonalInfoForm.tsx
@@ -1,5 +1,5 @@
 // EXTERNAL IMPORTS
-import React, { Dispatch, FunctionComponent, useCallback, memo } from 'react';
+import React, { Dispatch, FunctionComponent, useEffect, useCallback, memo } from 'react';
 import {
   Flex,
   Stack,
@@ -17,49 +17,53 @@ import { useForm, SubmitHandler } from 'react-hook-form';
 import { joiResolver } from '@hookform/resolvers/joi';
 
 // LOCAL IMPORTS
-import { PersonalInfoFormType } from '../../types';
+import { PersonalInfoFormType, PersonalInfoType } from '../../types';
 import { PersonalInfoFormSchema } from '../../schemas';
-import { VENDOR_STEPS } from '../../constants';
+import { CreateVendorStoreSteps } from '../../enums';
 import ProgressBar from '../ProgressBar';
 
 // Types
 type PropsType = {
-  setVendorStep: Dispatch<number>;
+  personalInfo: PersonalInfoType | null;
+  setPersonalInfo: Dispatch<PersonalInfoType | null>;
+  setStep: Dispatch<number>;
 };
 
 // Component
 const PersonalInfoForm: FunctionComponent<PropsType> = (props: PropsType) => {
-  const { register, formState, handleSubmit } = useForm<PersonalInfoFormType>({
-    resolver: joiResolver(PersonalInfoFormSchema)
+  const { register, formState, handleSubmit, setValue } = useForm<PersonalInfoFormType>({
+    resolver: joiResolver(PersonalInfoFormSchema),
+    defaultValues: {
+      name: props.personalInfo?.name,
+      address: props.personalInfo?.address,
+      city: props.personalInfo?.city,
+      state: props.personalInfo?.state,
+      code: props.personalInfo?.code
+    }
   });
+
+  useEffect((): void => {
+    if (props.personalInfo && 'name' in props.personalInfo) {
+      setValue('name', props.personalInfo.name);
+    }
+  }, [props.personalInfo]);
 
   const onPersonalInfoFormSubmit = useCallback<SubmitHandler<PersonalInfoFormType>>(
     async (data): Promise<void> => {
-      // const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_API_ROUTE}/auth/personal-info`, {
-      //   method: 'POST',
-      //   headers: {
-      //     Accept: 'application/json',
-      //     'Content-Type': 'application/json'
-      //   },
-      //   body: JSON.stringify({
-      //     first_name: data.first_name,
-      //     last_name: data.last_name,
-      //     month: data.month,
-      //     date: data.date,
-      //     year: data.year,
-      //     address1: data.address1,
-      //     address2: data.address2,
-      //     city: data.city,
-      //     country: data.country,
-      //     postal_code: data.postal_code
-      //   })
-      // });
-
-      if (true) {
-        props.setVendorStep(VENDOR_STEPS.VERIFY_PHONE_FORM);
-      } else {
-        console.log('ERROR: failed.');
-      }
+      props.setPersonalInfo({
+        name: data.name,
+        dateOfBirth: {
+          day: parseInt(data.day, 10),
+          month: parseInt(data.month, 10),
+          year: parseInt(data.year, 10)
+        },
+        address: data.address,
+        city: data.city,
+        country_id: parseInt(data.country, 10),
+        state: data.state,
+        code: data.code
+      });
+      props.setStep(CreateVendorStoreSteps.StoreInfoForm);
     },
     []
   );
@@ -72,28 +76,17 @@ const PersonalInfoForm: FunctionComponent<PropsType> = (props: PropsType) => {
       alignItems='center'
       spacing='4'
     >
-      <ProgressBar vendorStep={VENDOR_STEPS.PERSONAL_INFO_FORM} />
+      <ProgressBar step={CreateVendorStoreSteps.PersonalInfoForm} />
       <Flex width='100%' height='fit-content' direction='column' alignItems='center'>
         <Heading textAlign='center'>Personal Information</Heading>
         <Text textAlign='center'>Let's get started! Tell us about yourself.</Text>
       </Flex>
       <Stack direction={['column', 'column', 'row']} width={{ base: '80%', sm: '75%', md: '70%' }}>
         <Box flex='1'>
-          <FormControl
-            isInvalid={formState.errors.first_name?.message && formState.touchedFields.first_name}
-          >
-            <FormLabel>First Name</FormLabel>
-            <Input id='first_name' type='text' {...register('first_name')} />
-            <FormErrorMessage>{formState.errors.first_name?.message}</FormErrorMessage>
-          </FormControl>
-        </Box>
-        <Box flex='1'>
-          <FormControl
-            isInvalid={formState.errors.last_name?.message && formState.touchedFields.last_name}
-          >
-            <FormLabel>Last Name</FormLabel>
-            <Input id='last_name' type='text' {...register('last_name')} />
-            <FormErrorMessage>{formState.errors.last_name?.message}</FormErrorMessage>
+          <FormControl isInvalid={formState.errors.name?.message && formState.touchedFields.name}>
+            <FormLabel>Name</FormLabel>
+            <Input id='name' type='text' {...register('name')} />
+            <FormErrorMessage>{formState.errors.name?.message}</FormErrorMessage>
           </FormControl>
         </Box>
       </Stack>
@@ -102,29 +95,35 @@ const PersonalInfoForm: FunctionComponent<PropsType> = (props: PropsType) => {
           <FormControl isInvalid={formState.errors.month?.message && formState.touchedFields.month}>
             <FormLabel>Date of Birth</FormLabel>
             <Select placeholder='Month' id='month' {...register('month')}>
-              <option>October</option>
+              {getMonths().map((month: string, index: number) => (
+                <option value={index + 1}>{month}</option>
+              ))}
             </Select>
             <FormErrorMessage>{formState.errors.month?.message}</FormErrorMessage>
           </FormControl>
         </Box>
         <Box flex='1'>
           <FormControl
-            mt={{ base: '8', md: '8', sm: '4'}}
-            isInvalid={formState.errors.date?.message && formState.touchedFields.date}
+            mt={{ base: '8', md: '8', sm: '4' }}
+            isInvalid={formState.errors.day?.message && formState.touchedFields.day}
           >
-            <Select placeholder='Date' {...register('date')}>
-              <option>1</option>
+            <Select placeholder='Day' {...register('day')}>
+              {getDays().map((day: number) => (
+                <option value={day}>{day}</option>
+              ))}
             </Select>
-            <FormErrorMessage>{formState.errors.date?.message}</FormErrorMessage>
+            <FormErrorMessage>{formState.errors.day?.message}</FormErrorMessage>
           </FormControl>
         </Box>
         <Box flex='1'>
           <FormControl
-            mt={{ base: '8', md: '8', sm: '4'}}
+            mt={{ base: '8', md: '8', sm: '4' }}
             isInvalid={formState.errors.year?.message && formState.touchedFields.year}
           >
             <Select placeholder='Year' {...register('year')}>
-              <option>1998</option>
+              {getYears().map((year: number) => (
+                <option value={year}>{year}</option>
+              ))}
             </Select>
             <FormErrorMessage>{formState.errors.year?.message}</FormErrorMessage>
           </FormControl>
@@ -133,20 +132,11 @@ const PersonalInfoForm: FunctionComponent<PropsType> = (props: PropsType) => {
 
       <Box width={{ base: '80%', sm: '75%', md: '70%' }}>
         <FormControl
-          isInvalid={formState.errors.address1?.message && formState.touchedFields.address1}
+          isInvalid={formState.errors.address?.message && formState.touchedFields.address}
         >
-          <FormLabel>Address1</FormLabel>
-          <Input id='address1' type='text' {...register('address1')} />
-          <FormErrorMessage>{formState.errors.address1?.message}</FormErrorMessage>
-        </FormControl>
-      </Box>
-      <Box width={{ base: '80%', sm: '75%', md: '70%' }}>
-        <FormControl
-          isInvalid={formState.errors.address2?.message && formState.touchedFields.address2}
-        >
-          <FormLabel>Address2</FormLabel>
-          <Input id='address2' type='text' {...register('address2')} />
-          <FormErrorMessage>{formState.errors.address2?.message}</FormErrorMessage>
+          <FormLabel>Address</FormLabel>
+          <Input id='address' type='text' {...register('address')} />
+          <FormErrorMessage>{formState.errors.address?.message}</FormErrorMessage>
         </FormControl>
       </Box>
       <Stack direction={['column', 'column', 'row']} width={{ base: '80%', sm: '75%', md: '70%' }}>
@@ -163,7 +153,9 @@ const PersonalInfoForm: FunctionComponent<PropsType> = (props: PropsType) => {
           >
             <FormLabel>Country</FormLabel>
             <Select id='country' defaultValue='Canada' {...register('country')}>
-              <option>Canada</option>
+              {getCountries().map((country: string, index: number) => (
+                <option value={index}>{country}</option>
+              ))}
             </Select>
             <FormErrorMessage>{formState.errors.country?.message}</FormErrorMessage>
           </FormControl>
@@ -171,21 +163,17 @@ const PersonalInfoForm: FunctionComponent<PropsType> = (props: PropsType) => {
       </Stack>
       <Stack direction={['column', 'column', 'row']} width={{ base: '80%', sm: '75%', md: '70%' }}>
         <Box flex='1'>
-          <FormControl
-            isInvalid={formState.errors.province?.message && formState.touchedFields.province}
-          >
-            <FormLabel>Province</FormLabel>
-            <Input id='province' type='text' {...register('province')} />
-            <FormErrorMessage>{formState.errors.province?.message}</FormErrorMessage>
+          <FormControl isInvalid={formState.errors.state?.message && formState.touchedFields.state}>
+            <FormLabel>Province/State</FormLabel>
+            <Input id='state' type='text' {...register('state')} />
+            <FormErrorMessage>{formState.errors.state?.message}</FormErrorMessage>
           </FormControl>
         </Box>
         <Box flex='1'>
-          <FormControl
-            isInvalid={formState.errors.postal_code?.message && formState.touchedFields.postal_code}
-          >
+          <FormControl isInvalid={formState.errors.code?.message && formState.touchedFields.code}>
             <FormLabel>Postal/Zip Code</FormLabel>
-            <Input id='postal_code' type='text' {...register('postal_code')} />
-            <FormErrorMessage>{formState.errors.postal_code?.message}</FormErrorMessage>
+            <Input id='code' type='text' {...register('code')} />
+            <FormErrorMessage>{formState.errors.code?.message}</FormErrorMessage>
           </FormControl>
         </Box>
       </Stack>
@@ -194,6 +182,40 @@ const PersonalInfoForm: FunctionComponent<PropsType> = (props: PropsType) => {
       </Button>
     </Stack>
   );
+};
+
+const getMonths = (): string[] => {
+  let months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+  return months;
+};
+
+const getDays = (): number[] => {
+  let days = [];
+  for (let i = 1; i < 32; i++) days.push(i);
+  return days;
+};
+
+const getYears = (): number[] => {
+  let years = [];
+  for (let i = new Date().getFullYear() - 18; i >= 1980; i--) years.push(i);
+  return years;
+};
+
+const getCountries = (): string[] => {
+  return ['Canada', 'U.S.A.'];
 };
 
 // Display Name
