ac26dee65aa5a5984a0da8b671848fe26a4bebe5
AJ Clemente <aj.clemente@sootds.com>
2021-08-19 21:05:44 -0400

    Comment out verify phone form for now

--- src/containers/CreateVendorStore/components/index.ts
+++ src/containers/CreateVendorStore/components/index.ts
@@ -1,5 +1,5 @@
 export { default as PersonalInfoForm } from './PersonalInfoForm';
-export { default as VerifyPhoneForm } from './VerifyPhoneForm';
-export { default as StoreForm } from './StoreForm';
+// export { default as VerifyPhoneForm } from './VerifyPhoneForm';
+export { default as StoreInfoForm } from './StoreInfoForm';
 export { default as BillingForm } from './BillingForm';
 export { default as ProgressBar } from './ProgressBar';
