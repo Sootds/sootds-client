b17df259271e15e402815555868b964d3b6f8907
AJ Clemente <aj.clemente@sootds.com>
2021-08-19 21:01:25 -0400

    Update progress bar conditional statements

--- src/containers/CreateVendorStore/components/ProgressBar/ProgressBar.tsx
+++ src/containers/CreateVendorStore/components/ProgressBar/ProgressBar.tsx
@@ -1,29 +1,49 @@
-import React, { Dispatch, FunctionComponent, memo } from 'react';
+// EXTERNAL IMPORTS
+import React, { FunctionComponent, memo } from 'react';
 import { Flex, Circle, Progress } from '@chakra-ui/react';
 import { CheckIcon } from '@chakra-ui/icons';
 
+// LOCAL IMPORTS
+import { CreateVendorStoreSteps } from '../../enums';
+
 // Types
 type PropsType = {
-  vendorStep: number;
+  step: number;
 };
 
 const ProgressBar: FunctionComponent<PropsType> = (props: PropsType) => {
   return (
     <Flex justify='center' align='center' width='80%' mb={3}>
       <Circle size='35px' bg='gray.500' color='white'>
-        {props.vendorStep > 0 ? <CheckIcon /> : 1}
+        {props.step > CreateVendorStoreSteps.PersonalInfoForm ? <CheckIcon /> : 1}
       </Circle>
-      <Progress colorScheme='gray' value={props.vendorStep > 0 ? 100 : 0} width='full' height='2px' marginX={1} />
-      <Circle size='35px' bg={props.vendorStep > 0 ? 'gray.500' : 'gray.200'} color={props.vendorStep > 0 && 'white'}>
-      {props.vendorStep > 1 ? <CheckIcon /> : 2}
+      <Progress
+        colorScheme='gray'
+        value={props.step > CreateVendorStoreSteps.PersonalInfoForm ? 100 : 0}
+        width='full'
+        height='2px'
+        marginX={1}
+      />
+      <Circle
+        size='35px'
+        bg={props.step > CreateVendorStoreSteps.StoreForm ? 'gray.500' : 'gray.200'}
+        color={props.step > CreateVendorStoreSteps.StoreForm && 'white'}
+      >
+        {props.step > CreateVendorStoreSteps.StoreForm ? <CheckIcon /> : 2}
       </Circle>
-      <Progress colorScheme='gray' value={props.vendorStep > 1 ? 100 : 0} width='full' height='2px' marginX={1} />
-      <Circle size='35px' bg={props.vendorStep > 1 ? 'gray.500' : 'gray.200'} color={props.vendorStep > 1 && 'white'}>
-      {props.vendorStep > 2 ? <CheckIcon /> : 3}
-      </Circle>
-      <Progress colorScheme='gray' value={props.vendorStep > 2 ? 100 : 0} width='full' height='2px' marginX={1} />
-      <Circle size='35px' bg={props.vendorStep > 2 ? 'gray.500' : 'gray.200'} color={props.vendorStep > 2 && 'white'}>
-      {props.vendorStep > 3 ? <CheckIcon /> : 4}
+      <Progress
+        colorScheme='gray'
+        value={props.step > CreateVendorStoreSteps.StoreForm ? 100 : 0}
+        width='full'
+        height='2px'
+        marginX={1}
+      />
+      <Circle
+        size='35px'
+        bg={props.step > CreateVendorStoreSteps.BillingForm ? 'gray.500' : 'gray.200'}
+        color={props.step > CreateVendorStoreSteps.BillingForm && 'white'}
+      >
+        {props.step > CreateVendorStoreSteps.BillingForm ? <CheckIcon /> : 3}
       </Circle>
     </Flex>
   );
