8001805fafb45535037da037a5eb3635c435d0be
AJ Clemente <aj.clemente@sootds.com>
2021-08-25 16:34:32 -0400

    Add create vendor store API route to constants

--- src/shared/constants/apiRoutes.ts
+++ src/shared/constants/apiRoutes.ts
@@ -3,3 +3,5 @@ export const POST_AUTH_REFRESH_TOKEN = `${process.env.NEXT_PUBLIC_SERVER_API_ROU
 export const POST_AUTH_SIGNOUT = `${process.env.NEXT_PUBLIC_SERVER_API_ROUTE}/auth/signout`;
 export const POST_AUTH_FORGOT_PASSWORD = `${process.env.NEXT_PUBLIC_SERVER_API_ROUTE}/auth/forgot-password`;
 export const POST_AUTH_NEW_PASSWORD = `${process.env.NEXT_PUBLIC_SERVER_API_ROUTE}/auth/confirm-new-password`;
+
+export const POST_VENDORS_STORES_CREATE_STORE = `${process.env.NEXT_PUBLIC_SERVER_API_ROUTE}/vendors/stores`;
