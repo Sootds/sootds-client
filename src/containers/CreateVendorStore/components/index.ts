6c569bfd477685ead3812bd2c8ff2ca29dacb3cc
AJ Clemente <aj.clemente@sootds.com>
2021-12-15 22:11:50 -0500

    Remove non-existent import

--- src/containers/CreateVendorStore/components/index.ts
+++ src/containers/CreateVendorStore/components/index.ts
@@ -1,6 +1,5 @@
 export { default as PersonalInfoForm } from './PersonalInfoForm';
 export { default as StoreInfoForm } from './StoreInfoForm';
-export { default as BillingForm } from './BillingForm';
 export { default as BillingInfoForm } from './BillingInfoForm';
 export { default as ProgressBar } from './ProgressBar';
 export { default as CreateVendorStoreComplete } from './CreateVendorStoreComplete';
