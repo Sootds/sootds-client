675f32a9364bda2a79aa4336b9d033973fd158d7
AJ Clemente <aj.clemente@sootds.com>
2022-01-04 16:55:24 -0500

    Create `/settings` route page

---
+++ src/pages/settings.tsx
@@ -0,0 +1,26 @@
+// EXTERNAL IMPORTS
+import React, { FunctionComponent, memo, useContext, useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+// SHARED IMPORTS
+import { AuthContext } from '../shared/context';
+
+// LOCAL IMPORTS
+import { Settings } from '../containers';
+
+// Component
+const SettingsPage: FunctionComponent = () => {
+  const router = useRouter();
+  const authContext = useContext(AuthContext);
+
+  useEffect((): void => {
+    if (!authContext.user) router.push('/');
+  }, [authContext.user]);
+
+  return authContext.user ? <Settings username={authContext.user.username} accessToken={authContext.accessToken} /> : null;
+};
+
+// Display Name
+SettingsPage.displayName = SettingsPage.name;
+
+export default memo(SettingsPage);
