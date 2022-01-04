bc7a2fe41202538502eabaac81e458e46712bcc9
AJ Clemente <aj.clemente@sootds.com>
2022-01-04 16:54:34 -0500

    Add new API endpoints

--- src/shared/constants/apiRoutes.ts
+++ src/shared/constants/apiRoutes.ts
@@ -4,5 +4,10 @@ export const POST_AUTH_SIGNOUT = `${process.env.NEXT_PUBLIC_SERVER_API_ROUTE}/au
 export const POST_AUTH_FORGOT_PASSWORD = `${process.env.NEXT_PUBLIC_SERVER_API_ROUTE}/auth/forgot-password`;
 export const POST_AUTH_NEW_PASSWORD = `${process.env.NEXT_PUBLIC_SERVER_API_ROUTE}/auth/confirm-new-password`;
 
+export const POST_USER_INFO = `${process.env.NEXT_PUBLIC_SERVER_API_ROUTE}/users`;
+export const PUT_UPDATE_USER_INFO = `${process.env.NEXT_PUBLIC_SERVER_API_ROUTE}/users`;
+
 export const GET_VENDORS_STORES_GET_STORE = `${process.env.NEXT_PUBLIC_SERVER_API_ROUTE}/vendors/stores/?url_name=`;
 export const POST_VENDORS_STORES_CREATE_STORE = `${process.env.NEXT_PUBLIC_SERVER_API_ROUTE}/vendors/stores`;
+
+export const GET_COUNTRIES = `${process.env.NEXT_PUBLIC_SERVER_API_ROUTE}/utils/countries`;
